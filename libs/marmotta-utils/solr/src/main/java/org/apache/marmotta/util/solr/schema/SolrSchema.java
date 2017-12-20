547d8e52d4933cfd3d7e4eb9ef05ef3032285f06
dglachs <dglachs@gmail.com>
2017-12-20 13:13:30 +0100

    represents the current schema

--- libs/marmotta-utils/solr/src/main/java/org/apache/marmotta/util/solr/schema/SolrSchema.java
+++ libs/marmotta-utils/solr/src/main/java/org/apache/marmotta/util/solr/schema/SolrSchema.java
@@ -1,6 +1,5 @@
 package org.apache.marmotta.util.solr.schema;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -8,6 +7,12 @@ import java.util.List;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.schema.FieldTypeRepresentation;
 
+/**
+ * Represents a solr for a given core. Given the schema is created with a {@link SolrCoreAdministration} 
+ * object, chnges to the schema may be applied.
+ * @author dglachs
+ *
+ */
 public class SolrSchema {
     // private final SolrCoreConfiguration config;
     private final String coreName;
@@ -21,6 +26,7 @@ public class SolrSchema {
     public SolrSchema(String coreName) {
         this.coreName = coreName;
     }
+
     public SolrSchema(String coreName, SolrCoreAdministration admin) {
         this(coreName);
         this.admin = admin;
@@ -34,6 +40,7 @@ public class SolrSchema {
         }
         return null;
     }
+
     public SolrField getDynamicField(String name) {
         for (SolrField f : dynamicFields) {
             if (name.equals(f.getName())) {
@@ -42,6 +49,7 @@ public class SolrSchema {
         }
         return null;
     }
+
     public SolrField getDynamicFieldForType(String type) {
         for (SolrField f : dynamicFields) {
             if (type.equals(f.getType())) {
@@ -49,8 +57,9 @@ public class SolrSchema {
             }
         }
         return null;
-        
+
     }
+
     public SolrCopyField getCopyField(String name) {
         for (SolrCopyField f : copyFields) {
             if (name.equals(f.getSource())) {
@@ -59,84 +68,96 @@ public class SolrSchema {
         }
         return null;
     }
+
     public FieldTypeRepresentation getFieldType(String name) {
         for (FieldTypeRepresentation f : types) {
-            if (f.getAttributes()!= null && name.equals(f.getAttributes().get("name"))) {
+            if (f.getAttributes() != null && name.equals(f.getAttributes().get("name"))) {
                 return f;
             }
         }
         return null;
     }
+
     public boolean hasType(String name) {
         FieldTypeRepresentation t = getFieldType(name);
-        return t!=null;
+        return t != null;
     }
+
     public boolean hasField(String name) {
         SolrField f = getField(name);
         return f != null;
     }
+
     public boolean hasDynamicField(String name) {
         SolrField f = getDynamicField(name);
         return f != null;
     }
+
     public boolean hasDynamicFieldForType(String type) {
         SolrField f = getDynamicFieldForType(type);
         return f != null;
     }
+
     public boolean hasCopyField(String name) {
-        SolrCopyField f  = getCopyField(name);
-        return f !=null;
+        SolrCopyField f = getCopyField(name);
+        return f != null;
     }
+
     public boolean addField(SolrField field) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.addField(coreName, field);
     }
+
     /**
      * 
      * @param oldFields
      * @param newFields
      * @return
-     * @throws SolrServerException 
+     * @throws SolrServerException
      */
     public boolean updateSchema(List<SolrField> oldFields, List<SolrField> newFields) throws SolrServerException {
         SolrSchemaChange change = new SolrSchemaChange(this, oldFields);
         return change.updateSchema(newFields);
 
-        
     }
+
     public boolean updateSchema(List<SolrField> newFields) throws SolrServerException {
         SolrSchemaChange change = new SolrSchemaChange(this);
         return change.updateSchema(newFields);
     }
+
     boolean addFields(List<SolrField> toAdd) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.addFields(coreName, toAdd);
     }
+
     boolean updateFields(List<SolrField> toUpdate) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.updateFields(coreName, toUpdate);
     }
+
     boolean deleteFields(List<SolrField> toDelete) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.deleteFields(coreName, toDelete);
     }
 
     public boolean addCopyField(String source, String dest) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.addCopyField(coreName, source, Collections.singletonList(dest));
     }
+
     public boolean addDynamicField(SolrField defnition) throws SolrServerException {
-        if ( admin == null ) {
+        if (admin == null) {
             throw new IllegalStateException("No Admin Client");
         }
         return admin.addDynamicField(coreName, defnition);
@@ -149,25 +170,29 @@ public class SolrSchema {
     public void setTypes(List<FieldTypeRepresentation> types) {
         this.types = types;
     }
+
     public List<SolrField> getFields() {
         return fields;
     }
+
     public void setFields(List<SolrField> fields) {
         this.fields = fields;
     }
+
     public List<SolrField> getDynamicFields() {
         return dynamicFields;
     }
+
     public void setDynamicFields(List<SolrField> dynamicFields) {
         this.dynamicFields = dynamicFields;
     }
+
     public List<SolrCopyField> getCopyFields() {
         return copyFields;
     }
+
     public void setCopyFields(List<SolrCopyField> copyFields) {
         this.copyFields = copyFields;
     }
-
-
 
 }
